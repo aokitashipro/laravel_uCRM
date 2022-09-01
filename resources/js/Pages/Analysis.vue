bae047ec2b97d3be0ef1c814773da2b427dde871
aokitashipro <webmocha2@gmail.com>
2022-09-01 12:35:02 +0900

    add rfmRankTable

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -14,7 +14,10 @@ onMounted(() => {
 const form = reactive({
     startDate: null,
     endDate: null,
-    type: 'perDay'
+    type: 'perDay',
+    rfmPrms: [
+    14, 28, 60, 90, 7, 5, 3, 2, 300000, 200000, 100000, 30000
+  ],
 })
 
 const data = reactive({})
@@ -62,12 +65,54 @@ try{
                           <input type="radio" v-model="form.type" value="perMonth" ><span class="mr-4">月別</span>
                           <input type="radio" v-model="form.type" value="perYear" ><span class="mr-4">年別</span>
                           <input type="radio" v-model="form.type" value="decile" ><span class="mr-4">デシル分析</span>
+                          <input type="radio" v-model="form.type" value="rfm" ><span class="mr-4">RFM分析</span>
                           <br>
                           
                           From: <input type="date" name="startDate" v-model="form.startDate">
                             To: <input type="date" name="endDate" v-model="form.endDate"><br>
+
+                          <div v-if="form.type === 'rfm'" class="my-8">
+                            <table class="mx-auto">
+                              <thead>
+                              <tr>
+                                <th>ランク</th>
+                                <th>R (○日以内)</th>
+                                <th>F (○回以上)</th>
+                                <th>M (○円以上)</th>
+                              </tr>
+                              </thead>
+                              <tbody>
+                              <tr>
+                                <td>5</td>
+                                <td><input type="number" v-model="form.rfmPrms[0]"></td>
+                                <td><input type="number" v-model="form.rfmPrms[4]"></td>
+                                <td><input type="number" v-model="form.rfmPrms[8]"></td>
+                              </tr>
+                              <tr>
+                                <td>4</td>
+                                <td><input type="number" v-model="form.rfmPrms[1]"></td>
+                                <td><input type="number" v-model="form.rfmPrms[5]"></td>
+                                <td><input type="number" v-model="form.rfmPrms[9]"></td>
+                              </tr>
+                              <tr>
+                                <td>3</td>
+                                <td><input type="number" v-model="form.rfmPrms[2]"></td>
+                                <td><input type="number" v-model="form.rfmPrms[6]"></td>
+                                <td><input type="number" v-model="form.rfmPrms[10]"></td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
+                                <td><input type="number" v-model="form.rfmPrms[3]"></td>
+                                <td><input type="number" v-model="form.rfmPrms[7]"></td>
+                                <td><input type="number" v-model="form.rfmPrms[11]"></td>
+                              </tr>
+                              </tbody>
+                            </table>
+                          
+                          </div>
+
                             <button class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
-                        </form>
+                          </form>
 
                         <div v-show="data.data">
                         <Chart :data="data" />
